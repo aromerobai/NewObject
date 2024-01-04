a046a2193496694ae905e852cc52e7638fc765c5
aromerobai <aromerobai@uoc.edu>
2024-01-04 16:12:15 +0100

    Comienzo a desarrollar la gestion de pedidos

--- src/main/java/Vista/VistaAnadirPedido.java
+++ src/main/java/Vista/VistaAnadirPedido.java
@@ -1,24 +1,64 @@
 package Vista;
 
 import controlador.Controlador;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
 import java.io.IOException;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.sql.Date;
+
 public class VistaAnadirPedido {
     private Scene scene;
     private Stage stage;
     private Parent root;
+    @FXML
+    private TextField id_pedido;
+    @FXML
+    private TextField nif;
+    @FXML
+    private TextField id_articulo;
+    @FXML
+    private TextField cantidad;
+    @FXML
+    private ChoiceBox<String> estadoChoiceBox;
     private Controlador controlador;
 
     public void setControlador(Controlador controlador) {
         this.controlador = controlador;
         System.out.println("El controlador es" + controlador.toString());
+    }
+    @FXML
+    public void initialize() {
+        estadoChoiceBox.getItems().addAll("PENDIENTE", "ENVIADO");
+        // Puedes establecer un valor predeterminado si lo deseas
+        estadoChoiceBox.setValue("PENDIENTE");
+    }
+    @FXML
+    public void handleAnadirPedido(ActionEvent event) {
+
+        String idPedidoHandle = id_pedido.getText();
+        String nifHandle = nif.getText();
+        String idArticuloHandle = id_articulo.getText();
+        String cantidadHandle = cantidad.getText();
+        String estadoHandle = estadoChoiceBox.getValue();
+
+        LocalDateTime fechaActual = LocalDateTime.now();
+
+        try{
+            controlador.agregarPedido(idPedidoHandle, nifHandle, idArticuloHandle, Integer.parseInt(cantidadHandle), Date.from(fechaActual.toInstant(ZoneOffset.UTC)), estadoHandle);
+        }catch (Exception e){
+            System.out.println("Error al guardar los datos");
+        }
     }
 }
 
